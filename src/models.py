7feb45c01d0c3b092878ca24cd6d3aab7926b9d1
Marty Thane <mzf@tuta.io>
2024-11-06 18:14:48 +0100

    first draft of models

---
+++ src/models.py
@@ -0,0 +1,22 @@
+from neomodel import StructuredNode, UniqueIdProperty, StringProperty, DateTimeProperty, RelationshipTo
+from datetime import datetime
+
+class User(StructuredNode):
+    id = UniqueIdProperty()
+    username = StringProperty(unique_index=True, required=True, max_length=20)
+    password = StringProperty(required=True, max_length=64) # hash with sha256
+    posts = RelationshipTo("Post", "POSTS")
+    comments = RelationshipTo("Comment", "COMMENTS")
+    follows = RelationshipTo("User", "FOLLOWS")
+    likes = RelationshipTo("Post", "LIKES")
+
+class Post(StructuredNode):
+    id = UniqueIdProperty()
+    time = DateTimeProperty(default=datetime.now)
+    content = StringProperty(required=True)
+
+class Comment(StructuredNode):
+    id = UniqueIdProperty()
+    time = DateTimeProperty(default=datetime.now)
+    content = StringProperty(required=True)
+    commented_on = RelationshipTo("Post", "COMMENTED_ON")
