f91d68407d4483e98487c223e989c2d76c409fc3
Marty Thane <mzf@tuta.io>
2024-11-08 10:46:29 +0100

    added post and comment forms

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, TextAreaField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
@@ -7,3 +7,11 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired(),Length(min=8)])
     create_account = BooleanField("Create Account")
     submit = SubmitField("Login")
+
+class PostForm(FlaskForm):
+    content = TextAreaField("Content", validators=[DataRequired(), Length(max=256)])
+    submit = SubmitField("Post")
+
+class CommentForm(FlaskForm):
+    content = TextAreaField("Content", validators=[DataRequired(), Length(max=128)])
+    submit = SubmitField("Post")
