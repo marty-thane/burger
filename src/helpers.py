c3ea47cb3bfab918dcf9c234c40db75fef0424db
Marty Thane <mzf@tuta.io>
2024-11-11 15:53:00 +0100

    added logic for login and home

--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,20 @@
 from flask import redirect, url_for, session
+from neomodel import db
 import inspect
 
 # Return name of calling function
 def get_heading() -> str:
     return inspect.stack()[1].function
 
-def enforce_login() -> None:
-    if not session["user"]:
-        return redirect(url_for("login"))
+def get_feed():
+    query = """
+    MATCH (u:User)-[:POSTS]->(p:Post)
+    RETURN u.username AS author, p.time AS time, p.content AS content
+    ORDER BY p.time DESC
+    """
+    result = db.cypher_query(query)
+    return process_result(result)
 
+# Convert cypher query results into a list of dicts
+def process_result(result):
+    return [dict(zip(result[1], row)) for row in result[0]]
--- src/main.py
+++ src/main.py
@@ -1,45 +1,67 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session, flash
 from neomodel import config
 from forms import LoginForm, PostForm, CommentForm
 from models import User, Post, Comment
+from helpers import get_heading, get_feed
 import os
-import inspect
 
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 config.DATABASE_URL = f"bolt://neo4j:{os.getenv('NEO4J_PASSWORD')}@neo4j:7687"
 
-def get_heading():
-    return inspect.stack()[1].function
-
 @app.route("/login", methods=["GET", "POST"])
-def login():
+@app.route("/", methods=["GET", "POST"])
+def login() -> None:
     form = LoginForm()
     if form.validate_on_submit():
-        ...
-        return redirect(url_for("home"))
-    else:
-        return render_template("login.html", form=form)
+        username = form.username.data
+        password = form.password.data
+        create_account = form.create_account.data
+        if create_account:
+            user = User.nodes.get_or_none(username=username)
+            if not user:
+                user = User(username, password).save()
+                session["uid"] = user.uid
+                return redirect(url_for("home"))
+            else:
+                flash("This username is already taken.")
+                return redirect(url_for("login"))
+        else:
+            user = User.nodes.get_or_none(username=username, password=password)
+            if user:
+                session["uid"] = user.uid
+                return redirect(url_for("home"))
+            else:
+                flash("Wrong username or password.")
+                return redirect(url_for("login"))
+
+    return render_template("login.html", form=form)
 
 @app.route("/home", methods=["GET", "POST"])
-def home():
+def home() -> None:
     form = PostForm()
     if form.validate_on_submit():
-        ...
+        content = form.content.data
+        post = Post(content=content).save()
+        user = User.nodes.get(uid=session["uid"])
+        user.posts.connect(post)
         return redirect(url_for("home"))
-    return render_template("home.html", form=form, heading=get_heading())
+
+    posts = get_feed()
+
+    return render_template("home.html", form=form, heading=get_heading(), posts=posts)
 
 @app.route("/people")
-def people():
+def people() -> None:
     return render_template("people.html", heading=get_heading())
 
 @app.route("/post")
-def post():
+def post() -> None:
     return render_template("post.html", heading=get_heading())
 
 @app.route("/user")
-def user():
+def user() -> None:
     return render_template("user.html", heading=get_heading())
 
 if __name__ == "__main__":
