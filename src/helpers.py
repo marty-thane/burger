a9de9c799654a7771b4e16803c2398f1b04de005
Marty Thane <mzf@tuta.io>
2024-11-12 11:16:40 +0100

    removed redundant get_feed function

--- src/helpers.py
+++ src/helpers.py
@@ -6,15 +6,6 @@ import inspect
 def get_heading() -> str:
     return inspect.stack()[1].function
 
-def get_feed():
-    query = """
-    MATCH (u:User)-[:POSTS]->(p:Post)
-    RETURN u.username AS author, p.time AS time, p.content AS content
-    ORDER BY p.time DESC
-    """
-    result = db.cypher_query(query)
-    return process_result(result)
-
 # Convert cypher query results into a list of dicts
 def process_result(result):
     return [dict(zip(result[1], row)) for row in result[0]]
