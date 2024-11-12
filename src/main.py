f1da32b9cbfed26736d93d77846055a0d567c7b8
Marty Thane <mzf@tuta.io>
2024-11-12 11:18:03 +0100

    implemented home, user and post logic

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, session, flash
+from flask import Flask, render_template, redirect, url_for, session, flash, request
 from neomodel import config
 from forms import LoginForm, PostForm, CommentForm
 from models import User, Post, Comment
-from helpers import get_heading, get_feed
+from helpers import get_heading
 import os
 
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def login() -> None:
         if create_account:
             user = User.nodes.get_or_none(username=username)
             if not user:
-                user = User(username, password).save()
+                user = User(username=username, password=password).save()
                 session["uid"] = user.uid
                 return redirect(url_for("home"))
             else:
@@ -48,21 +48,44 @@ def home() -> None:
         user.posts.connect(post)
         return redirect(url_for("home"))
 
-    posts = get_feed()
+    users_posts = [(post.user.single(), post) for post in Post.nodes.order_by("-time").all()]
 
-    return render_template("home.html", form=form, heading=get_heading(), posts=posts)
+    return render_template("home.html", form=form, heading=get_heading(), users_posts=users_posts)
 
 @app.route("/people")
 def people() -> None:
     return render_template("people.html", heading=get_heading())
 
-@app.route("/post")
+@app.route("/post", methods=["GET", "POST"])
 def post() -> None:
-    return render_template("post.html", heading=get_heading())
+    post_uid = request.args.get("uid")
+    post = Post.nodes.get_or_none(uid=post_uid)
+    if not post:
+        return render_template("404.html", heading="Page not found")
+
+    form = CommentForm()
+    if form.validate_on_submit():
+        content = form.content.data
+        comment = Comment(content=content).save()
+        user = User.nodes.get(uid=session["uid"])
+        comment.user.connect(user)
+        comment.post.connect(post)
+        return redirect(url_for("post", uid=post_uid))
+
+    users_comments = [(comment.user.single(), comment) for comment in post.comments.order_by("-time").all()]
+
+    return render_template("post.html", form=form, heading=get_heading(), post=post, users_comments=users_comments)
 
 @app.route("/user")
 def user() -> None:
-    return render_template("user.html", heading=get_heading())
+    user_uid = request.args.get("uid")
+    user = User.nodes.get_or_none(uid=user_uid)
+    if not post:
+        return render_template("404.html", heading="Page not found")
+
+    posts = user.posts.order_by("-time").all()
+
+    return render_template("user.html", heading=get_heading(), user=user, posts=posts)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
