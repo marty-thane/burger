982e89c54f18fd80f88478716bbe3f4ba88c8eaf
Marty Thane <mzf@tuta.io>
2024-11-13 20:42:38 +0100

    models refactoring

--- src/models.py
+++ src/models.py
@@ -1,15 +1,15 @@
-from neomodel import StructuredNode, StructuredRel, StringProperty, UniqueIdProperty, DateTimeProperty, RelationshipTo, RelationshipFrom
+from neomodel import StructuredNode, StructuredRel, UniqueIdProperty, StringProperty, DateTimeProperty, RelationshipTo, RelationshipFrom
 from flask_login import UserMixin
 from datetime import datetime
 
 class User(StructuredNode, UserMixin):
     uid = UniqueIdProperty()
     username = StringProperty(unique_index=True, required=True, max_length=20)
-    password = StringProperty(required=True, max_length=64) # hash with sha256
+    password = StringProperty(required=True)
     posts = RelationshipTo("Post", "POSTED", model=StructuredRel)
     comments = RelationshipTo("Comment", "COMMENTED", model=StructuredRel)
     follows = RelationshipTo("User", "FOLLOWED", model=StructuredRel)
-    likes = RelationshipTo("Post", "LIKED", model=StructuredRel)
+    followers = RelationshipFrom("User", "FOLLOWED", model=StructuredRel)
     def is_active(self):
         return True
     def get_id(self):
